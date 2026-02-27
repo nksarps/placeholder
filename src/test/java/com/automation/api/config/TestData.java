8b3d09a372fb5f37275be4e7d2444dde9b22a19b
Nana Kwaku <nksarps@gmail.com>
2026-02-27 22:21:55 +0000

    Add API endpoint configuration and centralized test data classes

---
+++ src/test/java/com/automation/api/config/TestData.java
@@ -0,0 +1,18 @@
+package com.automation.api.config;
+
+/**
+ * Centralized test data (IDs and other reusable parameters) for API tests.
+ */
+public final class TestData {
+    private TestData() {}
+
+    public static final int DEFAULT_USER_ID = 1;
+    public static final int DEFAULT_POST_ID = 1;
+    public static final int DEFAULT_COMMENT_POST_ID = 1;
+    public static final int DEFAULT_ALBUM_ID = 1;
+    public static final int DEFAULT_PHOTO_ALBUM_ID = 1;
+    public static final int DEFAULT_TODO_USER_ID = 1;
+
+    public static final int INVALID_ID = 99999;
+}
+
